be121781f4a9d4049c30c8b0ca14bc91e5cf5bf8
winnubstj <winnubstj@janelia.hhmi.org>
2022-08-24 18:01:11 -0700

    Setting up

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # well-plate-assay
 Code for processing well plate assays
+
+# Install
+* Create Anaconda environment:
+    ```bash
+    conda env create -f environment.yml
+    ```
+* Install Module (in folder)
+    ```bash
+    pip install -e .
+    ```
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(
+    name = 'wellplate',
+    version = '0.0.1',
+    description = 'Tools for processing multi-well plate data',
+    url = 'https://github.com/e11bio/well-plate-assay',
+    author = 'Johan Winnubst',
+    author_email = 'winnubstj@janelia.hhmi.org',
+    packages = ['wellplate'],
+    zip_safe = False
+)
