c82c65627b15ceca15562ae171ea7e95719f53f7
winnubstj <winnubstj@janelia.hhmi.org>
2022-08-31 13:38:00 -0700

    Working test

--- app/plate_map.py
+++ app/plate_map.py
@@ -1,28 +1,41 @@
 import panel as pn
 import param
 import json
+from wellplate.elements import read_plate_xml
 
-class WellData(param.Parameterized):
-    with open('app/data.json') as f:
-        data_loc = json.load(f)
+def get_app():
+    class ExperimentData(param.Parameterized):
+        with open('app/data.json') as f:
+            data_info = json.load(f)
+        data_sets = data_info['data_sets']
+        exp_names = [loc['name'] for loc in data_sets]
+        current_exp_name = param.ObjectSelector(default=exp_names[0],objects=exp_names,label='Data')
+
+    exp_data = ExperimentData()
+
+    class PlateMap(param.Parameterized):
+        conditions = param.ObjectSelector(default='test',objects=['test','test2'],label='Data')
+        def view(self):
+            pass
     
-    names = [loc['name'] for loc in data_loc['data_sets']]
-    print(names)
-    data_set = param.ObjectSelector(default=names[0],objects=names,label='Data')
+    plate_map = PlateMap()
 
-well_data = WellData()
+    @pn.depends(exp_data.param.current_exp_name, watch=True)
+    def load_data(value):
+        data_index = exp_data.exp_names.index(exp_data.current_exp_name)
+        meta_data, meta_data_info, labels = read_plate_xml(exp_data.data_sets[data_index]['wellmap'])
+        # Get conditions.
+        conditions = [ cond for cond in meta_data.columns[1:] if cond not in ['Note','Notes']]
+        plate_map.param.conditions.objects = conditions
+    load_data(None)
 
-app = pn.template.MaterialTemplate(title='Plate Map')
 
-# Main Layout
-app.header.append(pn.Row(well_data.param.data_set , pn.layout.HSpacer()))
 
-data = 'hello'
+    app = pn.template.MaterialTemplate(title='Plate Map')
 
-# data selection
-#@pn.depends(select.value)
-#def load_data(value):
-#    data='test2'
+    # Main Layout
+    app.header.append(pn.Row(exp_data.param.current_exp_name , pn.layout.HSpacer()))
+    app.main.append(pn.Row(plate_map.param))
+    return app
 
-pn.serve(app,num_procs=1,threaded=False)
-#app.servable()
+get_app().servable()
