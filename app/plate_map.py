44d1681d2b81065b8f7bc66ada9e7aaa92737157
winnubstj <winnubstj@janelia.hhmi.org>
2022-09-05 13:36:41 -0700

    Better organization

--- app/plate_map.py
+++ app/plate_map.py
@@ -4,7 +4,7 @@ import json
 import numpy as np
 import pandas as pd
 
-from plate_map_plots import plot_plate_map, WellInfoTable
+from plate_map_plots import plot_plate_map, WellInfoTable, WellView, Channel
 
 from wellplate.elements import read_plate_xml, read_nd2
 import holoviews as hv
@@ -13,9 +13,6 @@ from matplotlib.colors import  to_hex
 def get_app():
     hv.extension('plotly')
     pn.config.throttled = True
-
-    ## Pane instances.
-    well_info_table = WellInfoTable(name='')
 
     class ExperimentData(param.Parameterized):
         with open('app/data.json') as f:
@@ -29,72 +26,30 @@ def get_app():
     class PlateMap(param.Parameterized):
         conditions = param.ObjectSelector(default='',objects=[''],label='Condition')
         meta_data=pd.DataFrame()
+        exp_data=None
         def view(self):
             return plot_plate_map(plate_map,well_view)
+        def load_experiment_data(self,current_exp_name):
+            print(current_exp_name)
+            data_index = self.exp_data.exp_names.index(current_exp_name)
+            meta_data, _, labels = read_plate_xml(self.exp_data.data_sets[data_index]['wellmap'])
+            plate_map.meta_data = meta_data
+            # Get conditions.
+            conditions = [ cond for cond in plate_map.meta_data.columns[1:] if cond not in ['Note','Notes']]
+            plate_map.param.conditions.objects = conditions
+            plate_map.conditions = conditions[0]
 
+    
+
+    # instantiate
     plate_map = PlateMap(name='')
-
-    def hook(plot, element):
-        fig = plot.state
-        fig['layout']['xaxis_visible']=False
-        fig['layout']['yaxis_visible']=False
-
-    class WellView(param.Parameterized):
-        xarr = None
-        im_size = None
-        selected_well = param.Number(0,label='Enable Brightfield Channel',precedence=-1)
-        channels = []
-        # 
-        rgb_result_im = None
-        redraw_flag = param.Boolean(False,label='Enable Brightfield Channel', precedence=-1)
-        def redraw(self):
-            self.redraw_flag = not self.redraw_flag
-        def change_selected_well(self, attr, old, new):
-            if len(new)>0:
-                self.selected_well = new[0]
-                # update well info.
-                well_info_table.well_info = plate_map.meta_data.iloc[self.selected_well].to_frame().transpose()
-        def create_result_rgb(self):
-            result_im = np.zeros((self.im_size[0],self.im_size[1],3),np.float)
-            for channel in self.channels:
-                if channel.result_rgb is not None:
-                    result_im += channel.result_rgb
-            return hv.RGB(np.clip(result_im,0,1)).opts(hooks=[hook])
+    plate_map.exp_data = exp_data
+    plate_map.bound = pn.bind(plate_map.load_experiment_data, exp_data.param.current_exp_name)
     well_view = WellView()
-
-
-    class Channel():
-        def __init__(self,name,colormap):
-            self.enable = pn.widgets.Checkbox(name=f'Enable {name}', value=True)
-            self.range = pn.widgets.RangeSlider(name='Display Range', start = 0, end= 2**16,
-                                value=(0,20000))
-            self.name = name
-            self.colormap = colormap
-            self.im_rgb = None
-            self.result_rgb = None
-            self.callback = None
-        def set_data(self, array):
-            self.range.start = array.min()
-            self.range.end = array.max()
-            #normalize.
-            norm_im = array/ (2**16)
-            # apply colormap.
-            self.im_rgb = self.colormap(norm_im)
-            # bind controls.
-            self.callback = pn.bind(self.set_img_range, self.enable, self.range, watch=True)
-        def set_img_range(self, enable,range, redraw=True ):
-            if enable:
-                if self.im_rgb is not None:
-                    # scale threshold to 0-1.
-                    low_lim = (range[0]/2**16)
-                    high_lim = (range[1]/2**16)
-                    im = self.im_rgb
-                    im = (im - low_lim) / (high_lim - low_lim)
-                    self.result_rgb = im[:,:,:3]
-            else:
-                self.result_rgb = None
-            if redraw:
-                well_view.redraw()
+    well_info_table = WellInfoTable(name='')
+    well_info_table.plate_map = plate_map
+    # add callback for well selection.
+    well_info_table.bound = pn.bind(well_info_table.selection_change, well_view.param.selected_well)
 
     @pn.depends(selected_well = well_view.param.selected_well, watch=True)
     def get_well_data(selected_well):
@@ -119,11 +74,11 @@ def get_app():
     # Header.
     app.header.append(pn.Row(exp_data.param.current_exp_name , pn.layout.HSpacer()))
     # Plate map.
-    app.main.append(pn.Column(plate_map.param,))
+    app.main.append(pn.Column(plate_map.bound,plate_map.param,))
     app.main.append(pn.Row(pn.layout.HSpacer(),plate_map.view,pn.layout.HSpacer()))
 
     # Image widgets.
-    app.main.append(pn.Column(well_info_table.param,well_info_table.view))
+    app.main.append(pn.Column(well_info_table.bound,well_info_table.param,well_info_table.view))
     channel_widgets_column = pn.Column()
     app.main.append(pn.Row(pn.layout.HSpacer(),channel_widgets_column,
         img_dmap.opts(width=700, height=700),pn.layout.HSpacer()))
@@ -131,19 +86,13 @@ def get_app():
     @pn.depends(exp_data.param.current_exp_name, watch=True)
     def load_experiment_data(value):
         data_index = exp_data.exp_names.index(exp_data.current_exp_name)
-        meta_data, _, labels = read_plate_xml(exp_data.data_sets[data_index]['wellmap'])
-        plate_map.meta_data = meta_data
-        # Get conditions.
-        conditions = [ cond for cond in plate_map.meta_data.columns[1:] if cond not in ['Note','Notes']]
-        plate_map.param.conditions.objects = conditions
-        plate_map.conditions = conditions[0]
         # load imaging data.
         well_view.xarr, names, colormaps = read_nd2(exp_data.data_sets[data_index]['nd2'])
         well_view.im_size = [well_view.xarr.shape[2],well_view.xarr.shape[3]]
         # create channels.
         well_view.channels.clear()
         for channel, channel_name in enumerate(names):
-            well_view.channels.append(Channel(channel_name, colormaps[channel]))
+            well_view.channels.append(Channel(channel_name, colormaps[channel], well_view))
         # Create controls.
         channel_widgets_column.clear()
         for channel in well_view.channels:
--- app/plate_map_plots.py
+++ app/plate_map_plots.py
@@ -7,6 +7,32 @@ from bokeh.transform import factor_cmap, linear_cmap
 from bokeh.models import TapTool
 import panel as pn
 import pandas as pd
+import holoviews as hv
+
+class WellView(param.Parameterized):
+    xarr = None
+    im_size = None
+    selected_well = param.Number(0,label='Enable Brightfield Channel',precedence=-1)
+    well_change_callback = []
+    channels = []
+    # 
+    rgb_result_im = None
+    redraw_flag = param.Boolean(False,label='Enable Brightfield Channel', precedence=-1)
+    def redraw(self):
+        self.redraw_flag = not self.redraw_flag
+    def change_selected_well(self, attr, old, new):
+        if len(new)>0:
+            self.selected_well = new[0]
+    def create_result_rgb(self):
+        result_im = np.zeros((self.im_size[0],self.im_size[1],3),np.float)
+        for channel in self.channels:
+            if channel.result_rgb is not None:
+                result_im += channel.result_rgb
+        return hv.RGB(np.clip(result_im,0,1)).opts(hooks=[self.hook])
+    def hook(self, plot, element):
+        fig = plot.state
+        fig['layout']['xaxis_visible']=False
+        fig['layout']['yaxis_visible']=False
 
 # plots.
 def plot_plate_map(plate_map, well_view, well_size = 96):
@@ -41,6 +67,7 @@ def plot_plate_map(plate_map, well_view, well_size = 96):
 class WellInfoTable(param.Parameterized):
     #hidden.
     well_info = param.DataFrame(pd.DataFrame([{'Well':'', 'Condition':''}]),precedence=-1)
+    plate_map = None
     def view(self):
         html_str = "<h1>Selected Well</h1>"
         html_str += """
@@ -74,3 +101,40 @@ class WellInfoTable(param.Parameterized):
         html_str +="</tr></table><p>"
         html = pn.pane.HTML(html_str)
         return html
+    def selection_change(self,selected_well):
+        if self.plate_map.meta_data.size>0:
+            self.well_info = self.plate_map.meta_data.iloc[selected_well].to_frame().transpose()
+
+class Channel():
+    def __init__(self,name,colormap, well_view):
+        self.enable = pn.widgets.Checkbox(name=f'Enable {name}', value=True)
+        self.range = pn.widgets.RangeSlider(name='Display Range', start = 0, end= 2**16,
+                            value=(0,20000))
+        self.name = name
+        self.colormap = colormap
+        self.well_view = well_view
+        self.im_rgb = None
+        self.result_rgb = None
+        self.callback = None
+    def set_data(self, array):
+        self.range.start = array.min()
+        self.range.end = array.max()
+        #normalize.
+        norm_im = array/ (2**16)
+        # apply colormap.
+        self.im_rgb = self.colormap(norm_im)
+        # bind controls.
+        self.callback = pn.bind(self.set_img_range, self.enable, self.range, watch=True)
+    def set_img_range(self, enable,range, redraw=True ):
+        if enable:
+            if self.im_rgb is not None:
+                # scale threshold to 0-1.
+                low_lim = (range[0]/2**16)
+                high_lim = (range[1]/2**16)
+                im = self.im_rgb
+                im = (im - low_lim) / (high_lim - low_lim)
+                self.result_rgb = im[:,:,:3]
+        else:
+            self.result_rgb = None
+        if redraw:
+            self.well_view.redraw()
