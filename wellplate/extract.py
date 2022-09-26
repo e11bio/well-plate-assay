6125595539041886891cec8980cb557cb70a72a3
winnubstj <winnubstj@janelia.hhmi.org>
2022-09-26 15:57:12 -0700

    Added calculation of scaffold vs epitope ratios

--- wellplate/extract.py
+++ wellplate/extract.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import zarr
-from wellplate.elements import read_nd2
+import pandas as pd
+from wellplate.elements import read_nd2, well_ind_to_id
 from cellpose import models
 from tqdm.notebook import tqdm
 import shutil
@@ -65,3 +66,32 @@ def calculate_intensities_channel(nd2_file, cell_channel, int_channel, redo=Fals
       values = np.array([bg_pixs.mean(),bg_pixs.std()])
       well_group = output.require_group(bg_path.parents[0])
       array = well_group.require_dataset(bg_path.name,data=values,shape = values.shape,chunks=(10,), dtype='f')
+
+def calculate_scaffold_epi_ratios(nd2_file, meta_data, scaffold_channel, epi_channel, threshold_factor = 0.5):
+  # get result zarr file. 
+  result_file = nd2_file_2_zarr_result_file(nd2_file)
+  proc_data = zarr.open(result_file)
+  signal_data = []
+  for well_ind in tqdm(range(96),desc='Analyzing wells'):
+    # get scaffold intensities
+    scaffold_intensities = proc_data[f'cells/intensities/well {well_ind}/channel {scaffold_channel}'][:]
+    # get background values.
+    background_values = proc_data[f'cells/background/well {well_ind}/channel {scaffold_channel}'][:]
+    mean_background = background_values[0]
+    std_background = background_values[1]
+    # THRESHOLD.
+    threshold = mean_background+(threshold_factor*std_background)
+    sig_cells = np.argwhere(scaffold_intensities>threshold)
+    # get epi intensities.  
+    epi_intensities = proc_data[f'cells/intensities/well {well_ind}/channel {epi_channel}'][:]
+    # store info
+    signal_data.append({'well':well_ind_to_id(well_ind),'num_cells': scaffold_intensities.shape[0],
+                        'num_sig_cells':sig_cells.size,'scaffold_signal':scaffold_intensities[sig_cells].mean(),'epi_signal':epi_intensities[sig_cells].mean()})
+  # calculate ratios.
+  signal_data = pd.DataFrame(signal_data)
+  signal_data['ratio'] =  signal_data['epi_signal']/signal_data['scaffold_signal']
+  signal_data['perc_pos'] =  (signal_data['num_sig_cells']/signal_data['num_cells'])*100
+  signal_data = meta_data.merge(signal_data,on='well')
+  # set types.
+  signal_data = signal_data.astype({'num_cells': 'int32','num_sig_cells': 'int32'})
+  return signal_data
