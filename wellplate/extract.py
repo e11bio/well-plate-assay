e11e59059a1c483e22e70e40ac288488229afed7
winnubstj <winnubstj@janelia.hhmi.org>
2022-09-26 15:49:08 -0700

    Added mask fluorescence intensity code

--- wellplate/extract.py
+++ wellplate/extract.py
@@ -4,6 +4,8 @@ from wellplate.elements import read_nd2
 from cellpose import models
 from tqdm.notebook import tqdm
 import shutil
+from skimage.measure import regionprops,label
+import numpy as np
 
 def nd2_file_2_zarr_result_file(nd2_file):
   # Get zarr output file name.
@@ -30,3 +32,36 @@ def run_cellpose(nd2_file, cell_channel, flow_threshold=0.9, cellprob_threshold=
       # store mask data.
       well_group = output.require_group(mask_path.parents[0])
       array = well_group.require_dataset(mask_path.name,data=masks,shape = masks.shape,chunks=(5000,5000), dtype='i2')
+
+def calculate_intensities_channel(nd2_file, cell_channel, int_channel, redo=False):
+  # get result zarr file. 
+  result_file = nd2_file_2_zarr_result_file(nd2_file)
+  output = zarr.open(result_file)
+  # read nd2 file.
+  im_data, channel_names, colormaps = read_nd2(nd2_file)
+  # get intensity channel index.
+  channel_ind = channel_names.index(int_channel)
+  for well_ind in tqdm(range(im_data.shape[0]), desc='Processing well'):
+    # check if data is already present, unless redo is requested.
+    int_path = Path(f'cells/intensities/well {well_ind}/channel {int_channel}')
+    if (int_path not in output) | redo==True:
+      # remove previous data if present.
+      if (int_path in output) & redo==True: 
+          shutil.rmtree(result_file/int_path)
+      # get mask.
+      mask_im = output[f'cells/masks/well {well_ind}/channel {cell_channel}'][:]
+      # get image.
+      int_im = im_data[well_ind,channel_ind,:,:].to_numpy()
+      # get region intensities.
+      props = regionprops(mask_im, int_im)
+      values = np.array([region["mean_intensity"] for region in props])
+      # store.
+      well_group = output.require_group(int_path.parents[0])
+      array = well_group.require_dataset(int_path.name,data=values,shape = values.shape,chunks=(50000,), dtype='f')
+      # Background values.
+      bg_path = Path(f'cells/background/well {well_ind}/channel {int_channel}/')
+      if (bg_path in output) & redo==True: shutil.rmtree(result_file/bg_path)
+      bg_pixs = int_im[mask_im==0]
+      values = np.array([bg_pixs.mean(),bg_pixs.std()])
+      well_group = output.require_group(bg_path.parents[0])
+      array = well_group.require_dataset(bg_path.name,data=values,shape = values.shape,chunks=(10,), dtype='f')
